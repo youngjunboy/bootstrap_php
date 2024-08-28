22a137d9941af1479da25bfba551977d239eb7d4
youngboy <121610435+youngjunboy@users.noreply.github.com>
2024-08-28 12:35:25 +0900

    Add files via upload

---
+++ action_page.php
@@ -0,0 +1,9 @@
+<h1>Welcome~!</h1>
+아이디 : <?php echo $_POST["uid"]; ?><br>
+비밀번호 : <?php echo $_POST["upwd"]; ?><br>
+졸업년도 : <?php echo $_POST["uyear"]; ?><br>
+학번 : <?php echo $_POST["uhakbun"]; ?><br>
+폰 번호 : <?php echo $_POST["utel"]; ?><br>
+이메일 : <?php echo $_POST["uemail"]; ?><br>
+집 주소 : <?php echo $_POST["uaddr"]; ?><br>
+기타 : <?php echo $_POST["uetc"]; ?><br>
